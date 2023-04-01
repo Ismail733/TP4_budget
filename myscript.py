68c435b28e43a0ff7623df51f7a4a3fbdd921e65
Ismail733 <78562971+Ismail733@users.noreply.github.com>
2023-04-01 13:01:27 -0400

    version script 5

--- myscript.py
+++ myscript.py
@@ -1,8 +1,3 @@
 import os
-
-good_hash = 'e4cfc6f'
-bad_hash = 'c1a4be0'
-
-os.system(f"git bisect start {bad_hash} {good_hash}")
-
+os.system('git bisect start $badhash $goodhash')
 os.system('git bisect run python manage.py test')
